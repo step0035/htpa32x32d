5674f7e918a16a2014c2278a8dc6d2f738c59808
step0035 <step0035@e.ntu.edu.sg>
2021-03-11 01:47:47 +0800

    stopped at eeprom thoffset_ij, also added caching

--- htpa.py
+++ htpa.py
@@ -3,6 +3,7 @@ from sensordef import *
 import time
 import numpy as np
 import struct
+import os
 
 def read_eeprom(ee_regADD):
     board.i2c_write(EEPROM_ADDRESS, [ee_regADD>>8, ee_regADD])
@@ -48,16 +49,34 @@ def main(board):
     board.set_pin_mode_i2c()
     time.sleep(5)
 
+    #set sensor characteristics
+    number_row = 32
+    number_col = 32
+    number_blocks = 8
+    number_pixel = 1024
+
     #read eeprom data
     global eeprom
-    print("Reading EEPROM data...")
-    for i in range(0x1F3F):
-        read_eeprom(i)
+
+    if os.path.isfile("./eeprom.txt"):
+        with open('eeprom.txt', 'r') as filehandle:
+            filecontents = filehandle.readlines()
+            for line in filecontents:
+                value = int(line[:-1]) # remove last character which is newline
+                eeprom.append(value)
+    else:
+        print("Reading EEPROM data...")
+        for i in range(0x1F3F):
+            read_eeprom(i)
+
+        with open('eeprom.txt', 'w') as filehandle:
+            for value in eeprom:
+                filehandle.write('%s\n' %value)
 
     print(eeprom)
     print(len(eeprom))
 
-    id = eeprom[E_ID4]<<24 | eeprom[E_ID3]<<16|eeprom[E_ID2]<<8|eeprom[E_ID1]
+    id = eeprom[E_ID4]<<24 | eeprom[E_ID3]<<16 | eeprom[E_ID2]<<8 | eeprom[E_ID1]
 
     mbit_calib = eeprom[E_MBIT_CALIB]
     bias_calib = eeprom[E_BIAS_CALIB]
@@ -84,18 +103,67 @@ def main(board):
     tablenumber = eeprom[E_TABLENUMBER2]<<8 | eeprom[E_TABLENUMBER1]
     arraytype = eeprom[E_ARRAYTYPE]
 
-    ptatgr_float = to_float(eeprom[E_PTATGR_4]<<24 | eeprom[E_PTATGR_3]<<16|eeprom[E_PTATGR_2]<<8|eeprom[E_PTATGR_1])
-    ptatoff_float = to_float(eeprom[E_PTATOFF_4]<<24 | eeprom[E_PTATOFF_3]<<16|eeprom[E_PTATOFF_2]<<8|eeprom[E_PTATOFF_1])
+    ptatgr_float = to_float(eeprom[E_PTATGR_4]<<24 | eeprom[E_PTATGR_3]<<16 | eeprom[E_PTATGR_2]<<8 | eeprom[E_PTATGR_1])
+    ptatoff_float = to_float(eeprom[E_PTATOFF_4]<<24 | eeprom[E_PTATOFF_3]<<16 | eeprom[E_PTATOFF_2]<<8 | eeprom[E_PTATOFF_1])
 
-    pixcmax = to_float(eeprom[E_PIXCMAX_4]<<24 | eeprom[E_PIXCMAX_3]<<16|eeprom[E_PIXCMAX_2]<<8|eeprom[E_PIXCMAX_1])
-    pixcmin = to_float(eeprom[E_PIXCMIN_4]<<24 | eeprom[E_PIXCMIN_3]<<16|eeprom[E_PIXCMIN_2]<<8|eeprom[E_PIXCMIN_1])
+    pixcmax = to_float(eeprom[E_PIXCMAX_4]<<24 | eeprom[E_PIXCMAX_3]<<16 | eeprom[E_PIXCMAX_2]<<8 | eeprom[E_PIXCMAX_1])
+    pixcmin = to_float(eeprom[E_PIXCMIN_4]<<24 | eeprom[E_PIXCMIN_3]<<16 | eeprom[E_PIXCMIN_2]<<8 | eeprom[E_PIXCMIN_1])
 
     epsilon = eeprom[E_EPSILON]
     globaloff = eeprom[E_GLOBALOFF]
     globalgain = eeprom[E_GLOBALGAIN_2]<<8 | eeprom[E_GLOBALGAIN_1]
 
-    #for i in range(nrofdefpix):
+    #-----DeadPixAdr-----
+    deadpixadr = np.zeros(nrofdefpix)
+    for i in range(nrofdefpix):
+        deadpixadr[i] = eeprom[E_DEADPIXADR + 2 * i + 1]<<8 | eeprom[E_DEADPIXADR + 2 * i]
+        if deadpixadr[i]>512:
+            deadpixadr[i] = 1024 + 512 - deadpixadr[i] + 2 * (deadpixadr[i] % 32) - 32
 
+    #-----DeadPixMask-----
+    deadpixmask = np.zeros(nrofdefpix)
+    for i in range(nrofdefpix):
+        deadpixmask[i] = eeprom[E_DEADPIXMASK + i]
+
+    #-----Thgrad_ij-----
+    m = 0
+    n = 0
+    addr_i = 0x0740 #start address
+    thgrad = np.zeros(shape=(32,32))
+
+    #top half
+    for i in range(512):
+        addr_i = 0x0740 + 2 * i
+        thgrad[m][n] = eeprom[E_THGRAD + 2 * i + 1]<<8 | eeprom[E_THGRAD + 2 * i]
+        n += 1
+        if n==32:
+            n = 0
+            m += 1
+
+    #bottom half
+    for i in range(number_col):
+        thgrad[31][i] = eeprom[E_THGRAD + 0x0400 + 2 * i + 1]<<8 | eeprom[E_THGRAD + 0x0400 + 2 * i]
+        thgrad[30][i] = eeprom[E_THGRAD + 0x0400 + 1 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0400 + 1 * 64 + 2 * i]
+        thgrad[29][i] = eeprom[E_THGRAD + 0x0400 + 2 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0400 + 2 * 64 + 2 * i]
+        thgrad[28][i] = eeprom[E_THGRAD + 0x0400 + 3 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0400 + 3 * 64 + 2 * i]
+
+        thgrad[27][i] = eeprom[E_THGRAD + 0x0500 + 2 * i + 1]<<8 | eeprom[E_THGRAD + 0x0500 + 2 * i]
+        thgrad[26][i] = eeprom[E_THGRAD + 0x0500 + 1 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0500 + 1 * 64 + 2 * i]
+        thgrad[25][i] = eeprom[E_THGRAD + 0x0500 + 2 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0500 + 2 * 64 + 2 * i]
+        thgrad[24][i] = eeprom[E_THGRAD + 0x0500 + 3 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0500 + 3 * 64 + 2 * i]
+
+        thgrad[23][i] = eeprom[E_THGRAD + 0x0600 + 2 * i + 1]<<8 | eeprom[E_THGRAD + 0x0600 + 2 * i]
+        thgrad[22][i] = eeprom[E_THGRAD + 0x0600 + 1 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0600 + 1 * 64 + 2 * i]
+        thgrad[21][i] = eeprom[E_THGRAD + 0x0600 + 2 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0600 + 2 * 64 + 2 * i]
+        thgrad[20][i] = eeprom[E_THGRAD + 0x0600 + 3 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0600 + 3 * 64 + 2 * i]
+
+        thgrad[19][i] = eeprom[E_THGRAD + 0x0700 + 2 * i + 1]<<8 | eeprom[E_THGRAD + 0x0700 + 2 * i]
+        thgrad[18][i] = eeprom[E_THGRAD + 0x0700 + 1 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0700 + 1 * 64 + 2 * i]
+        thgrad[17][i] = eeprom[E_THGRAD + 0x0700 + 2 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0700 + 2 * 64 + 2 * i]
+        thgrad[16][i] = eeprom[E_THGRAD + 0x0700 + 3 * 64 + 2 * i]<<8 | eeprom[E_THGRAD + 0x0700 + 3 * 64 + 2 * i]
+
+    #-----Thoffset_ij-----
+    
 
     #wake sensor
     #wake_sensor()
